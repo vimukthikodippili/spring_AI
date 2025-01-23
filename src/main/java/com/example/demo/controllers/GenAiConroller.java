634ab39fcb74aa0e582c77466c23c54baf9578c4
vimukthikodippili <kavimukthikodippili@email.com>
2025-01-24 00:06:56 +0530

    celebrity api

--- src/main/java/com/example/demo/controllers/GenAiConroller.java
+++ src/main/java/com/example/demo/controllers/GenAiConroller.java
@@ -2,11 +2,15 @@ package com.example.demo.controllers;
 
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -22,5 +26,23 @@ public class GenAiConroller {
         return chatClient.prompt(message)
                 .call()
                 .content();
+    }
+    @GetMapping("/celeb")
+    public String getCelebDetails(@RequestParam String name){
+        String message = """
+                list the details of the famus personility {name}
+                along with their carrier achievements and show the details in readable format
+                """;
+        PromptTemplate template  = new PromptTemplate(message);
+        Prompt prompt =template.create(
+                Map.of("name",name)
+
+        );
+return chatClient.prompt(prompt)
+        .call()
+        .chatResponse()
+        .getResult()
+        .getOutput()
+        .getContent();
     }
 }
